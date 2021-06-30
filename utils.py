9a23d9a4e0c4bd16dccb32e527fea24572a75596
yjthay <thayyeejie@gmail.com>
2021-06-30 12:06:49 +0100

    Rearranged such that class objects comes before functions Update such that sigmoid is applied on the last output layer of our model Layers
    Created 2 more functions namely predict and mlb_confusion_matrix
    
    - predict takes Pytorch model and list of text and run multi-label predictions with default threshold of 0.5.  All sample predictions are stacked and output as a single numpy array of (n_samples, m_classes)
    
    - mlb_confusion_matrix outputs a dictionary of emotions and confusion matrix associated with them. Each confusion matrix is normalised on "true" predictions and is m_classes by m_classes

--- utils.py
+++ utils.py
@@ -13,47 +13,23 @@ import pickle
 import itertools
 from sklearn.preprocessing import MultiLabelBinarizer
 
-from datasets import load_dataset,list_datasets
+from datasets import load_dataset, list_datasets
 import torch
 from torch.utils.data import Dataset, DataLoader
-from transformers import BertModel, BertTokenizer, RobertaModel, RobertaTokenizer
+from transformers import BertModel, BertTokenizer
+from sklearn.metrics import confusion_matrix
 
-def best_model_filename(folder_path):
-    regex_condition = re.compile('^.+_(.+\..+)\.pt')
-    # regex_condition = re.compile('^.+_(.+)_.+.pt')
-    min_val_loss = np.inf
-    # min_val_loss = 0
-    model_name = ''
-    for filename in os.listdir(folder_path):
-        if filename.endswith('.pt'):
-            val_loss = re.findall(regex_condition, filename)
-            if val_loss==[]:
-                continue
-            val_loss = float(val_loss[0])
-            if val_loss<min_val_loss:
-            # if val_loss>min_val_loss:
-                min_val_loss=val_loss
-                model_name = filename
-    return model_save_path+model_name
-
-def pickle_dump(data, fname):
-    with open(fname, 'wb') as f:
-        pickle.dump(data, f)
-
-def pickle_load(fname):
-    with open(fname, 'rb') as f:
-        data = pickle.load(f)
-    return data
 
 # Load Reddit comments from list of list (from HuggingFace) run them through BERT Tokenzier and Model, using them to transform raw text input into PyTorch tensor
 class EmotionsDataset(Dataset):
-    def __init__(self, data_all, Model=BertModel, Tokenizer=BertTokenizer, max_length=12, bert_type='bert-base-cased', device='cuda'):
+    def __init__(self, data, Model=BertModel, Tokenizer=BertTokenizer, max_length=12, bert_type='bert-base-cased',
+                 device='cuda'):
         self.tokenizer = Tokenizer.from_pretrained(bert_type)
         self.model = Model.from_pretrained(bert_type).to(device)
         self.device = device
 
         # Loading the data into text and labels
-        text, labels = data_all['text'], data_all['labels']
+        text, labels = data['text'], data['labels']
 
         # Tokenize the text and extract input_ids and attention_mask (ignoring
         tokens = self.tokenizer(text, padding='max_length', truncation=True, max_length=max_length, return_tensors="pt")
@@ -61,29 +37,22 @@ class EmotionsDataset(Dataset):
         attention_mask = tokens['attention_mask'].to(device)
 
         # Using sklearn MultiLabelBinarizer to change labels of [[3],[2],[0],[1,3]] to [[0,0,0,1],[0,0,1,0],[1,0,0,0],[0,1,0,1]]
-        labels = self.label_multi_one_hot(labels).to(device)
+        labels = label_multi_one_hot(labels).to(device)
 
         self.input_ids = input_ids
         self.attention_mask = attention_mask
         self.labels = labels
-
-    @staticmethod
-    def label_multi_one_hot(list_of_list):
-        mlb = MultiLabelBinarizer()
-        index, columns = len(list_of_list), max(list_of_list)[0]+1
-        labels = pd.DataFrame(mlb.fit_transform(list_of_list), columns=range(columns), index=range(index))
-        labels = torch.tensor(labels.values, dtype=float)
-        return labels
 
     def __getitem__(self, index):
         # full-BERT forward prop
         x = self.model(input_ids=self.input_ids[None, index], attention_mask=self.attention_mask[None, index])[0]
         x = x.view(np.prod(x.shape)).detach()  # detach full-BERT computation graph
         y = self.labels[index]
-        return x, y
+        return x.float(), y.float()
 
     def __len__(self):
         return len(self.labels)
+
 
 # Fully connected layers after BERT
 class Layers(torch.nn.Module):
@@ -103,5 +72,94 @@ class Layers(torch.nn.Module):
         # last layer no activation
         layer = self.layers[-1]
         outputs = layer(x)
-        return outputs
+        sigmoid = torch.nn.Sigmoid()
+        return sigmoid(outputs)
 
+
+def best_model_filename(folder_path):
+    regex_condition = re.compile('^.+_(.+\..+)\.pt')
+    # regex_condition = re.compile('^.+_(.+)_.+.pt')
+    min_val_loss = np.inf
+    # min_val_loss = 0
+    model_name = ''
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.pt'):
+            val_loss = re.findall(regex_condition, filename)
+            if val_loss == []:
+                continue
+            val_loss = float(val_loss[0])
+            if val_loss < min_val_loss:
+                # if val_loss>min_val_loss:
+                min_val_loss = val_loss
+                model_name = filename
+    return model_save_path + model_name
+
+
+def pickle_dump(data, fname):
+    with open(fname, 'wb') as f:
+        pickle.dump(data, f)
+
+
+def pickle_load(fname):
+    with open(fname, 'rb') as f:
+        data = pickle.load(f)
+    return data
+
+
+def label_multi_one_hot(list_of_list):
+    mlb = MultiLabelBinarizer()
+    index, columns = len(list_of_list), max(list_of_list)[0] + 1
+    labels = pd.DataFrame(mlb.fit_transform(list_of_list), columns=range(columns), index=range(index))
+    labels = torch.tensor(labels.values, dtype=float)
+    return labels
+
+
+def mapping(data=load_dataset('go_emotions')):
+    label_mapping = {}
+    obj = data['train'].features['labels'].feature
+    num_classes = obj.num_classes
+    for i in range(num_classes):
+        label_mapping[i] = obj.int2str(i)
+    return label_mapping
+
+
+def predict(model, text, batch_size=32, threshold=0.5):
+    '''
+    outputs = torch.rand((3,3))
+    threshold = 0.5
+    y_pred = (outputs >= threshold)*torch.ones(outputs.shape)
+    '''
+    n = len(text)
+    loader = DataLoader(text, batch_size=batch_size)
+    pred = []
+    for x, y in loader:
+        outputs = model(x)
+        y_pred = (outputs >= threshold) * torch.ones(outputs.shape)
+        pred += y_pred.tolist()
+    return np.array(pred)
+
+
+def mlb_confusion_matrix(label_mapping, model, data, batch_size=32):
+    '''
+    :param label_mapping: dictionary of label_num to emotions mapping
+    :param model: Pytorch Model for prediction
+    :param data: Loaded data from Hugging Face GoEmotions
+    :param batch_size: int default=32
+    :return:
+    dictionary of emotions and confusion matrix associated with it
+    '''
+    text, labels = data['text'], data['labels']
+    y_pred = predict(model, text, batch_size=batch_size)
+    y_true = label_multi_one_hot(labels).numpy()
+    label_names = list(label_mapping.values())
+    m_classes = len(label_names)
+    confusion_matrix_dict = {}
+
+    for label_num in label_mapping.keys():
+        y_true_label = y_true[:, label_num]
+        y_pred_label = y_pred[:, label_num]
+        temp_cm = confusion_matrix(y_pred=y_pred_label, y_true=y_true_label, labels=label_names, normalize='true')
+        assert temp_cm.shape == (m_classes, m_classes)
+        confusion_matrix_dict[label_mapping[label_num]] = temp_cm
+
+    return confusion_matrix_dict
